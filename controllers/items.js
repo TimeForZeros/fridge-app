4053c13aaccd1880628cd8f21d9f228e58df1342
Victor Gonzalez <dazedbutamused@gmail.com>
2019-08-28 14:52:11 -0700

    Sorting WORKS!!!!

--- controllers/items.js
+++ controllers/items.js
@@ -58,17 +58,14 @@ function index(req, res, next) {
     ? { name: new RegExp(req.query.name, "i") }
     : {};
   // Default to sorting by name
-  let sortKey = req.query.sort || "name";
   Item.find(modelQuery)
-    .sort(sortKey)
+    .sort({'expirationDate': 1})
     .exec(function(err, items) {
       if (err) return next(err);
-      // Passing search values, name & sortKey, for use in the EJS
       res.render("items/index", {
         items,
         user: req.user,
         name: req.query.name
-        //     sortKey
       });
     });
 }
