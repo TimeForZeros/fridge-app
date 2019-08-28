1f28e8ae986b439f90052497a65a54dfa4c3a0e6
Victor Gonzalez <dazedbutamused@gmail.com>
2019-08-28 12:14:20 -0700

    More Styling!!!!

--- models/item.js
+++ models/item.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var itemSchema = new mongoose.Schema ({
 foodName: {type: String},
 foodType: {type: String},
-expirationDate: {type: Date},
+expirationDate: {type: String},
 location: {type: String},
 googleId: String
 },
